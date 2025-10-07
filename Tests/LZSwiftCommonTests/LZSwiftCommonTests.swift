6c02e2a4434709e192c64463c0d7eac8de673c94
Rick M <rmann@latencyzero.com>
2025-10-07 15:53:58 -0700

    Added unit test for optional assignment

--- Tests/LZSwiftCommonTests/LZSwiftCommonTests.swift
+++ Tests/LZSwiftCommonTests/LZSwiftCommonTests.swift
@@ -1,6 +1,29 @@
 import Testing
 @testable import LZSwiftCommon
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+
+
+
+
+
+@Test
+func
+optionalAssignment()
+	async
+	throws
+{
+	var foo = 123
+	var bar: Optional<Int> = 123
+	var newVal: Optional<Int> = nil
+	
+	foo ??= newVal
+	bar ??= newVal
+	#expect(foo == 123)
+	#expect(bar == 123)
+	
+	newVal = 321
+	foo ??= newVal
+	bar ??= newVal
+	#expect(foo == 321)
+	#expect(bar == 321)
 }
